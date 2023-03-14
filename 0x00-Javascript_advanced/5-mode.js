c75e7dee5a422bd0ca92c9561fdf7e3b6e927320
Brayden Vernon <4933@holbertonstudents.com>
2023-03-14 20:38:39 +0000

    adding task 5

---
+++ 0x00-Javascript_advanced/5-mode.js
@@ -0,0 +1,36 @@
+function changeMode(size, weight, transform, background, color) {
+  return () => {
+    document.body.style.fontSize = size;
+    document.body.style.fontWeight = weight;
+    document.body.style.textTransform = transform;
+    document.body.style.backgroundColor = background;
+    document.body.style.color = color;
+  };
+}
+
+function main() {
+  spooky = changeMode(9, 'bold', 'uppercase', 'pink', 'green');
+  darkMode = changeMode(12, 'bold' 'capitalize', 'black', 'white');
+  screamMode = changeMode(12, 'normal', 'lowercase', 'white', 'black');
+
+  let text = document.createElement('p');
+  text.textContent = 'The Fitness Gram Pacer Test is a multi-stage aerobic capacity test that gets more difficult as time continues.';
+  document.body.append(text);
+
+  let spookyButt = document.createElement('button');
+  spookyButt.textContent = 'Spoopy';
+  document.body.append(spookyButt);
+  spookyButt.addEventListener('click', spooky);
+
+  let darkButt = document.createElement('button');
+  darkButt.textContent = 'Dark mode';
+  document.body.append(darkButt);
+  darkButt.addEventListener('click', darkMode);
+
+  let screamButt = document.createElement('button');
+  screamButt.textContent = 'Scream mode';
+  document.body.append(screamButt);
+  screamButt.addEventListener('click', screamMode);
+}
+
+main();
